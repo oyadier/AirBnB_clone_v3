78953e80acf4c79f04abfdb6fa515d3f0b5704a8
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-02-26 21:28:57 +0000

    Handle conflict

---
+++ api/v1/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Creating a api using flask framework"""
+
+
+from flask import Flask, jsonify, Blueprint
+from models import storage
+from flask_cors import CORS
+from api.v1.views import app_views
+from os import getenv
+
+
+app = Flask(__name__)
+CORS(app, origins="0.0.0.0")
+app.register_blueprint(app_views)
+CORS(app_views)
+
+
+@app.teardown_appcontext
+def teardown(error):
+    "for forward routing"
+    storage.close
+
+
+if __name__ == "__main__":
+    hbnb_host = getenv('HBNB_API_HOST')
+    hbnb_port = getenv('HBNB_API_PORT')
+    app.run(host=hbnb_host, port=hbnb_port,
+            threaded=True, debug=True)
