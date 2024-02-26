3eddc83f4e79937fb2ad236d17d63b9bf3f6b30c
Nii-101 <noelamptey101@gmail.com>
2024-02-26 23:18:28 +0000

    Revrted to previous commit

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,6 +5,10 @@
 
 
 from flask import Blueprint
-from . import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+
